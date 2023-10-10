014a8090db01d48f241cb033e930c7df3c13575a
Mafestire <seabaraven@gmail.com>
2023-10-11 00:31:26 +0200

    icons & logo

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 // $(document).ready(function(){
-//     $('.next').on('click', function(){
+//         const $nextBtn = $('#next');
+
+//     $nextBtn.on('click', function(){
 //         const display = $('.active');
 //         const nextDisplay = display.next();
 
-//         if (nextDisplay.length) {
-//             display.removeClass('active').css('z-index', -10);
-//             nextDisplay.addClass('active'.css('z-index', 10))
-//         }
+        // if (nextDisplay.length) {
+        //     display.removeClass('active').css('z-index', -10);
+        //     nextDisplay.addClass('active'.css('z-index', 10))
+        // }
 //     });
 // });
+
 
 $(document).ready(function() {
 
     const $images = $('.slide');
     const $nextBtn = $('#next');
-    const slideWidth = $('.slide').width();
+    const slideWidth = $('.image').width();
+    const slideHight = $('.slide').height();
     let currentIndex = 0;
 
+    // $nextBtn.on('click', function() {
+    //     currentIndex = (currentIndex + 1) % $images.children().length;
+    //     updateSlider();
+    // });
+
     $nextBtn.on('click', function() {
+        console.log('Next button clicked'); // Add this line for debugging
         currentIndex = (currentIndex + 1) % $images.children().length;
         updateSlider();
     });
 
+    // function updateSlider() {
+    //     const translateY = -currentIndex * slideWidth;
+    //     $images.css('transform', `translateY(${translateY}px)`);
+    // }
+
     function updateSlider() {
-        const translateY = -currentIndex * slideWidth;
+        const translateY = -currentIndex * slideHight;
         $images.css('transform', `translateY(${translateY}px)`);
     }
 });
